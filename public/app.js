1fd5b27da2ea801d42d26fcc6c1f2ea845d2a235
tedro <sktedro@gmail.com>
2023-12-22 12:59:55 +0100

    Added chart 2 and axes labels to chart 1

--- public/app.js
+++ public/app.js
@@ -9,6 +9,8 @@ TODOs
 - chart 3
 */
 
+const maxChartHeight = window.innerHeight * 0.9;
+
 // Dictionary to translate device values
 const deviceTranslations = {
   'agx': 'Jetson AGX Xavier',
@@ -55,6 +57,15 @@ const resolutionToShape = {
   '352x192': 'star',
 };
 
+const mAPMetricString = {
+  "bbox_mAP": "mAP",
+  "bbox_mAP_50": "mAP@50",
+  "bbox_mAP_75": "mAP@75",
+  "bbox_mAP_s": "mAP (small)",
+  "bbox_mAP_m": "mAP (medium)",
+  "bbox_mAP_l": "mAP (large)",
+}
+
 // Quantization to fill/stroke mapping
 const quantizationToFillStroke = {
   'none': { fill: 'colored', stroke: 'black' },
@@ -68,8 +79,8 @@ const quantizations = Object.keys(quantizationTranslations);
 const resolutions = Object.keys(resolutionToShape);
 const batchSizes = ["1", "2", "4", "8", "16", "32"];
 const inferenceEngines = ["tensorrt", "onnxruntime"];
-const mAPMetrics = ["bbox_mAP", "bbox_mAP_50", "bbox_mAP_75", "bbox_mAP_s",
-    "bbox_mAP_m", "bbox_mAP_l"]
+// const mAPMetrics = ["bbox_mAP", "bbox_mAP_50", "bbox_mAP_75", "bbox_mAP_s", "bbox_mAP_m", "bbox_mAP_l"]
+const mAPMetrics = ["bbox_mAP_50", "bbox_mAP_75", "bbox_mAP_s", "bbox_mAP_m", "bbox_mAP_l", "bbox_mAP"]
 
 let selectedBatchSize = batchSizes[0];
 let selectedDevice = devices[0];
@@ -91,11 +102,17 @@ let selectedModelCombinations = [];
 let additionalFilter = {};
 let filteredModelCombinations = [];
 
+const dataPointRadius = 20;
 const chart1Div = d3.select("#chart-1");
 let chart1Svg = null;
 let chart1Tooltip = null;
 chart1Div.on('mousemove', (event) => {
-  const [x, y] = d3.pointer(event);
+  let [x, y] = d3.pointer(event);
+
+  // Subtract margins
+  x -= chart1Margin.left + 10;
+  y -= chart1Margin.top + 10;
+
   const xValue = chart1Svg.xScale.invert(x);
   const yValue = chart1Svg.yScale.invert(y);
 
@@ -108,6 +125,9 @@ chart1Div.on('mousemove', (event) => {
 chart1Div.on('mouseout', () => {
   chart1Tooltip.style('visibility', 'hidden');
 });
+
+const chart2Div = d3.select("#chart-2");
+let chart2Svg = null;
 
 const tableDiv = document.getElementById('table');
 
@@ -175,8 +195,8 @@ function getDataPointAttributes(model, resolution, quantization) {
 function createDataPointSVG(model, resolution, quantization) {
   const svgns = 'http://www.w3.org/2000/svg';
   const svg = document.createElementNS(svgns, 'svg');
-  svg.setAttribute('width', '20');
-  svg.setAttribute('height', '20');
+  svg.setAttribute('width', dataPointRadius.toString());
+  svg.setAttribute('height', dataPointRadius.toString());
 
   let shape, attributes;
   [shape, attributes] = getDataPointAttributes(model, resolution, quantization);
@@ -202,6 +222,7 @@ function selectionChanged(){
 
 function filterChanged(){
   drawChart1();
+  drawChart2();
 }
 
 /*
@@ -576,11 +597,16 @@ function updateAdditionalFilterTable() {
  * Chart 1
  */
 
+const chart1Margin = { top: 40, right: 20, bottom: 30, left: 40 };
 function drawChart1() {
-  const margin = { top: 20, right: 20, bottom: 30, left: 40 };
 
-  let width = parseInt(chart1Div.style("width")) - margin.left - margin.right;
-  let height = parseInt(chart1Div.style("width")) * 0.5 - margin.top - margin.bottom;
+  let svgWidth = parseInt(chart1Div.style("width")) - chart1Margin.left - chart1Margin.right;
+  let svgHeight = parseInt(chart1Div.style("width")) * 0.5 - chart1Margin.top - chart1Margin.bottom;
+  if(svgHeight > maxChartHeight){
+    let oldSvgHeight = svgHeight;
+    svgHeight = maxChartHeight;
+    svgWidth = svgWidth * (svgHeight / oldSvgHeight);
+  }
 
   if(chart1Svg){
     chart1Svg.remove();
@@ -590,15 +616,28 @@ function drawChart1() {
 
   chart1Svg = chart1Div.append("svg")
       // .attr("width", width + margin.left + margin.right) // like this, the chart div would keep getting larger on every call of this function, for some reason
-      .attr("width", width + margin.left)
-      .attr("height", height + margin.top + margin.bottom)
+      .attr("width", svgWidth + chart1Margin.left)
+      .attr("height", svgHeight + chart1Margin.top + chart1Margin.bottom)
     .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
+      .attr("transform", `translate(${chart1Margin.left},${chart1Margin.top})`);
+
+  // TODO Fix this - not a good placement.
+  // place FPS to the center and maybe mAP too? Margins would have to be increased
+  chart1Svg.append('text').text("mAP")
+    .attr('x', 0)
+    .attr('y', -20)
+    .attr('font-size', '1.1em')
+    .style('text-anchor', 'end')
+  chart1Svg.append('text').text("FPS")
+    .attr('x', svgWidth)
+    .attr('y', svgHeight + 30)
+    .attr('font-size', '1.1em')
+    .style('text-anchor', 'end')
 
   // Store scales and axes in the SVG for later access
-  chart1Svg.xScale = d3.scaleLog().range([0, width - margin.right]);
-  chart1Svg.yScale = d3.scaleLinear().range([height, 0]);
-  chart1Svg.xAxisGroup = chart1Svg.append("g").attr("transform", `translate(0,${height})`);
+  chart1Svg.xScale = d3.scaleLog().range([0, svgWidth - chart1Margin.right]);
+  chart1Svg.yScale = d3.scaleLinear().range([svgHeight, 0]);
+  chart1Svg.xAxisGroup = chart1Svg.append("g").attr("transform", `translate(0,${svgHeight})`);
   chart1Svg.yAxisGroup = chart1Svg.append("g");
 
   const filteredData = getFilteredData();
@@ -607,20 +646,23 @@ function drawChart1() {
   const maxX = d3.max(filteredData, d => d.fps) * 1.05;
   chart1Svg.xScale.domain([minX, maxX]);
 
-  const minY = d3.min(filteredData, d => d.mAP) * 0.90;
-  const maxY = d3.max(filteredData, d => d.mAP);
+  const minY = d3.min(filteredData, d => d.bbox_mAP) * 0.90;
+  const maxY = d3.max(filteredData, d => d.bbox_mAP);
   chart1Svg.yScale.domain([minY, maxY]);
 
   // Update axes
   const xAx = d3.axisBottom(chart1Svg.xScale)
   const yAx = d3.axisLeft(chart1Svg.yScale)
 
-  // TODO Gridlines:
-  // xAx.ticks(5).tickSize(-height)
-  // yAx.ticks(5).tickSize(-width)
-
-  chart1Svg.xAxisGroup.call(xAx);
-  chart1Svg.yAxisGroup.call(yAx);
+  let fontSize = "16px";
+  // chart1Svg.xAxisGroup.call(xAx);
+  chart1Svg.xAxisGroup.call(xAx)
+    .selectAll('.tick text')
+    .style('font-size', fontSize);
+  // chart1Svg.yAxisGroup.call(yAx);
+  chart1Svg.yAxisGroup.call(yAx)
+    .selectAll('.tick text')
+    .style('font-size', fontSize);
 
   // Bind data to SVG elements
   const points = chart1Svg.selectAll(".data-point").data(filteredData, d => d.combination);
@@ -629,7 +671,7 @@ function drawChart1() {
   points.enter()
     .append('g')
     .attr("class", "data-point")
-    .attr("transform", d => `translate(${chart1Svg.xScale(d.fps)}, ${chart1Svg.yScale(d.mAP)})`)
+    .attr("transform", d => `translate(${chart1Svg.xScale(d.fps) - dataPointRadius / 2}, ${chart1Svg.yScale(d.bbox_mAP) - dataPointRadius / 2})`)
     .each(function(d) {
       this.appendChild(createDataPointSVG(d.model, d.resolution, d.quantization));
     });
@@ -653,7 +695,12 @@ function getFilteredData() {
       resolution,
       quantization,
       fps: parseFloat(deviceData.fps),
-      mAP: parseFloat(deviceData.bbox_mAP),
+      bbox_mAP: parseFloat(deviceData.bbox_mAP),
+      bbox_mAP_50: parseFloat(deviceData.bbox_mAP_50),
+      bbox_mAP_75: parseFloat(deviceData.bbox_mAP_75),
+      bbox_mAP_s: parseFloat(deviceData.bbox_mAP_s),
+      bbox_mAP_m: parseFloat(deviceData.bbox_mAP_m),
+      bbox_mAP_l: parseFloat(deviceData.bbox_mAP_l),
       combination: [model, resolution, quantization].join(':') // Unique identifier
     };
   });
@@ -671,3 +718,271 @@ initializeAdditionalFilter();
 initDeviceSelection();
 initBatchSizeSelection();
 initModelResolutionQuantizationSelection();
+
+
+
+
+
+// SPIDER CHART
+function polarToCartesian(angle, radius) {
+  return {
+    x: radius * Math.cos(angle),
+    y: radius * Math.sin(angle)
+  };
+}
+function drawChart2(){
+
+  if(chart2Svg){
+    chart2Svg.remove();
+    chart2Svg = null;
+    chart2Div.select("svg").remove();
+  }
+
+  let svgHeight = maxChartHeight;
+  let svgWidth = 1.15 * svgHeight;
+  if(window.innerWidth < svgWidth){
+    svgWidth = window.innerWidth;
+    svgHeight = svgWidth / 1.15;
+  }
+  const svgRadius = svgWidth / 2;
+
+  chart2Svg = chart2Div.append("svg")
+      .attr("width", svgWidth + chart1Margin.left)
+      .attr("height", svgHeight + chart1Margin.top)
+    .append("g")
+      .attr("transform", `translate(${svgWidth / 2 + chart1Margin.left / 2},${svgHeight / 2 + chart1Margin.top / 2})`);
+
+  let notice = chart2Div.select(".notice");
+  let txt = "Please not that the mAP values for FP32 and FP16 are always the same. Therefore, if both are present, only FP16 is shown.";
+  if(notice){
+    notice.remove();
+  }
+  chart2Div.append("center").text(txt).attr("class", "notice").style("font-size", "0.9em").style("color", "gray");
+
+  const scales = [...Array(mAPMetrics.length)].map(() => d3.scaleLinear()
+    .range([0, svgRadius])
+    .domain([0, 0.85]));
+
+
+  // Bind data to SVG elements
+  let oldFilteredData = getFilteredData();
+  let filteredData = [];
+  for(let i = 0; i < oldFilteredData.length; i++){
+
+    // If the data point is FP32 and FP16 is also present (not filtered out) for the same combination, only show FP16
+    if(oldFilteredData[i].quantization === "none"){
+      let fp16Exists = oldFilteredData.some(d =>
+        d.quantization === "fp16" &&
+        d.model === oldFilteredData[i].model &&
+        d.resolution === oldFilteredData[i].resolution &&
+        d.device === oldFilteredData[i].device
+      );
+      // Skip the FP32 data point if FP16 exists
+      if (fp16Exists){
+        continue;
+      }
+    }
+
+    for(let j = 0; j < mAPMetrics.length; j++){
+      let old = {...oldFilteredData[i]};
+      let new_d = old;
+      new_d.map_metric = mAPMetrics[j];
+      new_d.map_metric_value = new_d[mAPMetrics[j]];
+      new_d.combination = new_d.combination + ":" + new_d.map_metric;
+      filteredData.push(new_d);
+    }
+  }
+  const points = chart2Svg.selectAll(".data-point").data(filteredData, d => d.combination);
+
+  function getDataPointCoords(d){
+    let mAP_metric = d.map_metric;
+    let value = d.map_metric_value;
+    let sliceAngle = Math.PI * 2 / mAPMetrics.length;
+    let angle = sliceAngle * mAPMetrics.indexOf(mAP_metric);
+    let radius = scales[mAPMetrics.indexOf(mAP_metric)](value)
+    return polarToCartesian(angle, radius);
+  }
+
+  // Enter new elements
+  points.enter()
+    .append('g')
+    .attr("class", "data-point")
+    .attr("transform", d => `translate(${getDataPointCoords(d).x - dataPointRadius / 2}, ${getDataPointCoords(d).y - dataPointRadius / 2})`)
+    .each(function(d) {
+      this.appendChild(createDataPointSVG(d.model, d.resolution, d.quantization));
+    });
+
+  /**
+  function calculateIntermediatePoints(start, end, numberOfPoints) {
+    let points = [];
+    for (let i = 1; i <= numberOfPoints; i++) {
+      let x = start.x + (end.x - start.x) * i / (numberOfPoints + 1);
+      let y = start.y + (end.y - start.y) * i / (numberOfPoints + 1);
+      points.push({ x, y });
+    }
+    return points;
+  }
+  // Connect points by dotted lines (dots in style of the data points)
+  let dataGroups = d3.group(filteredData, d => d.model + d.quantization + d.resolution);
+  dataGroups.forEach(group => {
+    // Convert group from Map to Array
+    let groupArray = Array.from(group);
+    groupArray.push(groupArray[0]); // Add first element to the end to close the loop
+
+    for (let i = 0; i < groupArray.length - 1; i++) {
+      let startData = groupArray[i];
+      let endData = groupArray[i + 1];
+
+      let startPoint = getDataPointCoords(startData);
+      let endPoint = getDataPointCoords(endData);
+
+      // Calculate intermediate points
+      let intermediatePoints = calculateIntermediatePoints(startPoint, endPoint, 5);
+
+      // Draw small dots at intermediate points
+      // intermediatePoints.forEach(point => {
+      //   chart2Svg.append('circle')
+      //     .attr('cx', point.x)
+      //     .attr('cy', point.y)
+      //     .attr('r', 2) // Small radius for dots
+      //     .style('fill', 'black');
+      // });
+      intermediatePoints.forEach(point => {
+        let smallDot = createDataPointSVG(startData.model, startData.resolution, startData.quantization);
+        chart2Svg.append(() => smallDot)
+          .attr('transform', `translate(${point.x}, ${point.y})`);
+      });
+    }
+  });
+  */
+  let dataGroups = d3.group(filteredData, d => d.model + d.quantization + d.resolution);
+  dataGroups.forEach(group => {
+    // Convert group from Map to Array
+    let groupArray = Array.from(group);
+    groupArray.push(groupArray[0]); // Add first element to the end to close the loop
+
+    for (let i = 0; i < groupArray.length - 1; i++) {
+      let startData = groupArray[i];
+      let endData = groupArray[i + 1];
+
+      let startPoint = getDataPointCoords(startData);
+      let endPoint = getDataPointCoords(endData);
+
+      let [svgShape, attribs] = getDataPointAttributes(startData.model, startData.resolution, startData.quantization);
+      let fill = attribs["fill"];
+      let stroke = attribs["stroke"];
+
+      if(fill === "transparent"){
+        chart2Svg.append('line')
+          .attr('x1', startPoint.x)
+          .attr('y1', startPoint.y)
+          .attr('x2', endPoint.x)
+          .attr('y2', endPoint.y)
+          .style('stroke', stroke)
+          .style('stroke-width', '1px');
+      }else if(stroke === "none"){
+        chart2Svg.append('line')
+          .attr('x1', startPoint.x)
+          .attr('y1', startPoint.y)
+          .attr('x2', endPoint.x)
+          .attr('y2', endPoint.y)
+          .style('stroke', fill)
+          .style('stroke-width', '3px');
+      }else{
+
+        chart2Svg.append('line')
+          .attr('x1', startPoint.x)
+          .attr('y1', startPoint.y)
+          .attr('x2', endPoint.x)
+          .attr('y2', endPoint.y)
+          .style('stroke', stroke)
+          .style('stroke-width', '3px');
+        chart2Svg.append('line')
+          .attr('x1', startPoint.x)
+          .attr('y1', startPoint.y)
+          .attr('x2', endPoint.x)
+          .attr('y2', endPoint.y)
+          .style('stroke', fill)
+          .style('stroke-width', '1px');
+      }
+    }
+  });
+
+  // Remove old elements
+  points.exit().remove();
+
+  // Draw axes
+  for (let i = 0; i < mAPMetrics.length; i++) {
+    const angle = (Math.PI * 2 / mAPMetrics.length) * i;
+    const lineCoords = polarToCartesian(angle, svgRadius);
+
+    // Add axes text:
+    chart2Svg.append('line')
+      .attr('x1', 0)
+      .attr('y1', 0)
+      .attr('x2', lineCoords.x)
+      .attr('y2', lineCoords.y)
+      .style('stroke', 'black')
+      .style('stroke-width', '1px');
+    if([1, 2, 4, 5].includes(i)){
+      chart2Svg.append('text')
+        .attr('x', lineCoords.x + 20)
+        .attr('y', lineCoords.y)
+        .text(mAPMetricString[mAPMetrics[i]]);
+    }else if (i == 0){
+      chart2Svg.append('text')
+        .attr('x', lineCoords.x)
+        .attr('y', lineCoords.y - 20)
+        .style('text-anchor', 'end')
+        .text(mAPMetricString[mAPMetrics[i]]);
+    }else{
+      chart2Svg.append('text')
+        .attr('x', lineCoords.x)
+        .attr('y', lineCoords.y - 20)
+        .text(mAPMetricString[mAPMetrics[i]]);
+    }
+  }
+
+  // Ticks and tick labels
+  const tickStep = 0.05;
+  const ticksPerAxis = 0.85 / tickStep;
+  for (let i = 0; i < mAPMetrics.length; i++) {
+    const angle = (Math.PI * 2 / mAPMetrics.length) * i;
+
+    for (let j = 1; j < ticksPerAxis; j++) { // skip 0
+      const tickValue = j * tickStep;
+      const tickCoords1 = polarToCartesian(angle-0.1/j, scales[i](tickValue)); // divide by j so each tick is the same length
+      const tickCoords2 = polarToCartesian(angle+0.1/j, scales[i](tickValue));
+      let maxX = d3.max([tickCoords1.x, tickCoords2.x]);
+      let maxY = d3.max([tickCoords1.y, tickCoords2.y]);
+
+      // Only add tick labels for every other tick
+      if(j % 2 === 0){
+        if ([1, 2, 4, 5].includes(i)) {
+          chart2Svg.append('text')
+            .attr('x', maxX + 10)
+            .attr('y', maxY + 2)
+            .text(tickValue.toFixed(1));
+        } else if ([0, 3].includes(i)) {
+          chart2Svg.append('text')
+            .attr('x', maxX)
+            .attr('y', maxY + dataPointRadius)
+            .style('text-anchor', 'middle')
+            .text(tickValue.toFixed(1));
+        }
+      }
+
+      // Tick line
+      chart2Svg.append('line')
+        .attr('x1', tickCoords1.x)
+        .attr('y1', tickCoords1.y)
+        .attr('x2', tickCoords2.x)
+        .attr('y2', tickCoords2.y)
+        .style('stroke', 'black')
+        .style('stroke-width', '1px');
+    }
+  }
+
+}
+
+
